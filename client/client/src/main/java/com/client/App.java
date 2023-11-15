0899d008c6ad308a63097d2b6d570970794eb6a0
lai2-24 <lai2-24@itismeucci.com>
2023-11-15 13:37:17 +0100

    added .gitignore

--- client/client/src/main/java/com/client/App.java
+++ client/client/src/main/java/com/client/App.java
@@ -1,5 +1,11 @@
 package com.client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,41 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+            System.out.println("CLIENT ESEGUITO");
+
+            Scanner scanner = new Scanner(System.in);
+            Socket s = new Socket("localhost", 3001);
+            BufferedReader inputDalServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream outputVersoServer = new DataOutputStream(s.getOutputStream());
+
+            String scelta = "";
+            String letteraoparola = "";
+
+            do {
+                System.out.println(inputDalServer.readLine());
+                System.out.println(inputDalServer.readLine());
+                // il client vuole indovinare la lettera o la parola?
+                scelta = scanner.nextLine();
+                outputVersoServer.writeBytes(scelta + "\n");
+                //stampa messaggio 'scrivi la lettera' ecc
+                System.out.println(inputDalServer.readLine());
+                // il client digita la lettera/parola
+                letteraoparola = scanner.nextLine();
+                outputVersoServer.writeBytes(letteraoparola + "\n");
+                //stampa la stringa nascosta mostrando il carattere indovinato
+                System.out.println(inputDalServer.readLine());
+                if(scelta.equals("X") || scelta.equals("x")) {
+                    break;
+                }
+            }while(true);
+
+            s.close();
+        }
+        catch(Exception e){
+            System.out.println("Errore di connessione");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
